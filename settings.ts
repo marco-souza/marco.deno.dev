338e8bbd8d0bca46698dcdae0bac2139f9dda0ae
Marco Antônio <ma.souza.junior@gmail.com>
2022-07-02 17:15:10 -0300

    enh: add github link

--- settings.ts
+++ settings.ts
@@ -2,4 +2,5 @@ export const site = {
   title: "marco.labs",
   description: "My personal experimentation site.",
   keywords: "Marco Antônio,software,projects,playground,deno,typescript",
+  repository: "https://github.com/marco-souza/marco.deno.dev",
 };
