aa092b5db4389ef702d51632fd7ffd3cbb669aa6
Marco Antônio <ma.souza.junior@gmail.com>
2022-07-02 14:58:04 -0300

    enh: improve SEO

--- routes/_app.tsx
+++ routes/_app.tsx
@@ -2,10 +2,20 @@
 import { h } from "preact";
 import { AppProps } from "$fresh/server.ts";
 import Layout from "~components/Layout.tsx";
+import { site } from "#/settings.ts";
 
 export default function App({ Component }: AppProps) {
+  const headers = (
+    <head>
+      <title>{site.title}</title>
+      <meta name="description" content={site.description} />
+      <meta name="keywords" content={site.keywords} />
+    </head>
+  );
+
   return (
     <Layout>
+      {headers}
       <Component />
     </Layout>
   );
---
+++ settings.ts
@@ -0,0 +1,5 @@
+export const site = {
+  title: "marco.labs",
+  description: "My personal experimentation site.",
+  keywords: "Marco Antônio,software,projects,playground,deno,typescript",
+};
