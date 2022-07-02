4b1499addcc4b35b7ffaa4cce9043ab1b551a9a8
Marco Antônio <ma.souza.junior@gmail.com>
2022-07-02 15:06:11 -0300

    enh: use fresh head

--- routes/_app.tsx
+++ routes/_app.tsx
@@ -1,16 +1,17 @@
 /** @jsx h */
 import { h } from "preact";
 import { AppProps } from "$fresh/server.ts";
+import { Head } from "$fresh/runtime.ts";
 import Layout from "~components/Layout.tsx";
 import { site } from "#/settings.ts";
 
 export default function App({ Component }: AppProps) {
   const headers = (
-    <head>
+    <Head>
       <title>{site.title}</title>
       <meta name="description" content={site.description} />
       <meta name="keywords" content={site.keywords} />
-    </head>
+    </Head>
   );
 
   return (
