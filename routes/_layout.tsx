63a6bdccead7ce415ec02f2b24aacca599bff77d
Marco Antônio <ma.souza.junior@gmail.com>
2023-09-12 14:52:02 -0300

    fea: add navbar menu

--- routes/_layout.tsx
+++ routes/_layout.tsx
@@ -15,22 +15,7 @@ export default defineLayout((_req, ctx) => {
 const NavBar = () => (
   <div class="navbar bg-base-100 md:space-between">
     <div class="flex-none">
-      <button class="btn btn-square btn-ghost">
-        <svg
-          xmlns="http://www.w3.org/2000/svg"
-          fill="none"
-          viewBox="0 0 24 24"
-          class="inline-block w-5 h-5 stroke-current"
-        >
-          <path
-            stroke-linecap="round"
-            stroke-linejoin="round"
-            stroke-width="2"
-            d="M4 6h16M4 12h16M4 18h16"
-          >
-          </path>
-        </svg>
-      </button>
+      <ExpandableMenu />
     </div>
 
     <div class="flex-1 flex justify-center">
@@ -41,4 +26,42 @@ const NavBar = () => (
       <ThemeSwitcher />
     </div>
   </div>
+);
+
+const ExpandableMenu = () => (
+  <ul class="menu menu-horizontal px-1">
+    <li>
+      <details>
+        <summary>
+          <MenuIcon />
+        </summary>
+
+        <ul class="p-2 bg-base-100 w-36">
+          <li>
+            <a>Link 1</a>
+          </li>
+          <li>
+            <a>Link 2</a>
+          </li>
+        </ul>
+      </details>
+    </li>
+  </ul>
+);
+
+const MenuIcon = () => (
+  <svg
+    xmlns="http://www.w3.org/2000/svg"
+    fill="none"
+    viewBox="0 0 24 24"
+    class="inline-block w-5 h-5 stroke-current"
+  >
+    <path
+      stroke-linecap="round"
+      stroke-linejoin="round"
+      stroke-width="2"
+      d="M4 6h16M4 12h16M4 18h16"
+    >
+    </path>
+  </svg>
 );
