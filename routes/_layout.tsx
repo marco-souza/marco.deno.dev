c310b69052279b88bb4c5b19fc9c902c2ab627b9
Marco Antônio <ma.souza.junior@gmail.com>
2023-09-12 15:04:42 -0300

    enh: add github and menu icon

--- routes/_layout.tsx
+++ routes/_layout.tsx
@@ -1,6 +1,8 @@
 import { defineLayout } from "$fresh/server.ts";
 import ThemeSwitcher from "#/islands/ThemeSwitcher.tsx";
 import { Logo } from "~/components/Logo.tsx";
+import GithubIcon from "~/icon/GitHubIcon.tsx";
+import { site } from "~/settings.ts";
 
 export default defineLayout((_req, ctx) => {
   return (
@@ -13,55 +15,60 @@ export default defineLayout((_req, ctx) => {
 });
 
 const NavBar = () => (
-  <div class="navbar bg-base-100 md:space-between">
-    <div class="flex-none">
+  <div class="navbar">
+    <div class="navbar-start">
       <ExpandableMenu />
     </div>
 
-    <div class="flex-1 flex justify-center">
+    <div class="navbar-center">
       <Logo />
     </div>
 
-    <div class="flex-none">
+    <div class="navbar-end">
       <ThemeSwitcher />
+
+      <a href={site.repository} target="_black" class="btn btn-ghost btn-circle">
+        <GithubIcon />
+      </a>
     </div>
   </div>
 );
 
 const ExpandableMenu = () => (
-  <ul class="menu menu-horizontal px-1">
-    <li>
-      <details>
-        <summary>
-          <MenuIcon />
-        </summary>
-
-        <ul class="p-2 bg-base-100 w-36">
-          <li>
-            <a>Link 1</a>
-          </li>
-          <li>
-            <a>Link 2</a>
-          </li>
-        </ul>
-      </details>
-    </li>
-  </ul>
+  <div class="dropdown" role="button" as="button">
+    <label tabIndex={0} class="btn btn-ghost btn-circle">
+      <MenuIcon />
+    </label>
+    <ul
+      tabIndex={0}
+      class="menu menu-sm dropdown-content mt-3 z-[1] p-2 shadow bg-base-100 rounded-box w-52"
+    >
+      <li>
+        <a>Homepage</a>
+      </li>
+      <li>
+        <a>Portfolio</a>
+      </li>
+      <li>
+        <a>About</a>
+      </li>
+    </ul>
+  </div>
 );
 
 const MenuIcon = () => (
   <svg
     xmlns="http://www.w3.org/2000/svg"
+    class="h-5 w-5"
     fill="none"
     viewBox="0 0 24 24"
-    class="inline-block w-5 h-5 stroke-current"
+    stroke="currentColor"
   >
     <path
       stroke-linecap="round"
       stroke-linejoin="round"
       stroke-width="2"
-      d="M4 6h16M4 12h16M4 18h16"
-    >
-    </path>
+      d="M4 6h16M4 12h16M4 18h7"
+    />
   </svg>
 );
