621ed392d81a2b46242fa8cfff271ff14986d6ae
Marco Antônio <ma.souza.junior@gmail.com>
2023-09-12 18:32:04 -0300

    cleanup

--- packages/configs/settings.ts
+++
@@ -1,6 +0,0 @@
-export const site = {
-  title: "marco.labs",
-  description: "My personal experimentation site.",
-  keywords: "Marco Antônio,software,projects,playground,deno,typescript",
-  repository: "https://github.com/marco-souza/marco.deno.dev",
-};
