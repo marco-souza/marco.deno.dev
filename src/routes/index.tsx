8bad14a5a258c2ba8ecd8977a23910493385ef53
Marco Antônio <ma.souza.junior@gmail.com>
2024-11-07 17:37:25 -0300

    fea: implement client side theme switcher

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -23,14 +23,36 @@ const Layout: FC = (props) => {
 const Top: FC<{ messages: string[] }> = (props: {
   messages: string[];
 }) => {
+  function toggleTheme() {
+    const htmlElement = document.documentElement;
+    const systemTheme =
+      globalThis.matchMedia("(prefers-color-scheme: light)")?.matches
+        ? "light"
+        : "dark";
+
+    const selectedTheme = htmlElement.dataset.theme ?? systemTheme;
+
+    htmlElement.dataset.theme = selectedTheme === "dark" ? "light" : "dark";
+  }
+
   return (
     <Layout>
       <h1>Hello Hono!</h1>
+
       <ul>
         {props.messages.map((message) => {
           return <li>{message}!!</li>;
         })}
       </ul>
+
+      <h1>Toggle Dark Mode</h1>
+      <button
+        id="toggle-theme"
+        class="btn btn-primary"
+        onClick={toggleTheme}
+      >
+        Toggle Theme
+      </button>
 
       <button
         hx-post="/clicked"
@@ -48,12 +70,14 @@ export function definePage(app: Hono) {
   app.get("/", (ctx: Context) => {
     console.log("GET /");
     const messages = ["Good Morning", "Good Evening", "Good Night"];
-    return ctx.html(<Top messages={messages} />);
+    return ctx.render(<Top messages={messages} />);
   });
 
   app.post("/clicked", (ctx: Context) => {
     const now = Date.now();
     console.log("POST /clicked", now);
-    return ctx.html(<p class="bg-red-800">Clicked now: {now}</p>);
+    return ctx.render(
+      <p class="rounded-full p-4 bg-red-800">Clicked now: {now}</p>,
+    );
   });
 }
