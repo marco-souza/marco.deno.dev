1de7ae5efe4b7b0d53ca7d13e27a40b7a8d5bab3
Marco Antônio <ma.souza.junior@gmail.com>
2024-11-11 19:45:48 -0300

    fix: error page text color

--- src/components/ErrorPage.tsx
+++ src/components/ErrorPage.tsx
@@ -10,14 +10,16 @@ export default function ErrorPage(props: Props) {
   return (
     <div class="my-[15vh] items-center flex flex-col gap-4">
       <div class="items-center justify-center flex flex-col gap-4">
-        <h1 class="text-9xl font-extrabold text-gray-700 dark:text-gray-100">
+        <h1 class="text-9xl font-extrabold text-gray-700 dark:text-gray-200">
           {props.code ?? 404}
         </h1>
         <h2 class="bg-pink-500 px-2 text-sm rounded rotate-12 absolute text-gray-700">
           {props.title ?? "Page Not Found"}
         </h2>
       </div>
-      <p class="text-sm text-gray-400">{props.description}</p>
+      <p class="text-sm text-gray-400">
+        {props.description}
+      </p>
       {props.children}
     </div>
   );
