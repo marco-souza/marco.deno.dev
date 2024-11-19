cf9daf05c09cd02700ffe69a988ab42a6febe0c4
Marco Antônio <ma.souza.junior@gmail.com>
2024-11-19 17:23:07 -0300

    enh: share gh profile card

--- src/components/GitHubProfile.tsx
+++ src/components/GitHubProfile.tsx
@@ -6,37 +6,45 @@ type Props = {
   profile: GitHubProfile;
 };
 
-export const GitHubProfileView: FC<Props> = ({ profile }) => {
+export const GitHubProfileCard: FC<Props> = ({ profile }) => (
+  <>
+    <div class="gap-2 flex flex-col">
+      <img
+        alt="It's Me"
+        src={profile.avatar_url}
+        class="h-40 mx-auto rounded-full"
+      />
+
+      <div class="text-3xl font-light">{profile.name}</div>
+    </div>
+
+    <p
+      class="opacity-70 font-extralight text-md px-8"
+      dangerouslySetInnerHTML={{ __html: parseBioText(profile.bio) }}
+    />
+  </>
+);
+
+export const GitHubProfileView: FC<Props> = (
+  { profile },
+) => {
   return (
     <div class="max-w-2xl px-8 mx-auto">
       <div class="flex flex-col gap-10 my-8 xl:my-16 2xl:my-32 items-center text-center">
-        <div class="gap-2 flex flex-col">
-          <img
-            alt="It's Me"
-            src={profile.avatar_url}
-            class="h-40 mx-auto rounded-full"
-          />
-
-          <div class="text-3xl font-light">{profile.name}</div>
-        </div>
-
-        <div
-          class="opacity-70 font-extralight text-md px-4 sm:px-20 md:px-32"
-          dangerouslySetInnerHTML={{ __html: parseBioText(profile.bio) }}
-        />
+        <GitHubProfileCard profile={profile} />
 
         <div class="grid gap-8 grid-cols-1 sm:mx-20 sm:grid-cols-2 w-full">
           <a
             href="#"
             class="btn btn-outline text-pink-400 hover:text-white hover:bg-pink-400 hover:animate-zoom-in"
           >
-            Primary
+            Let's have a Coffee ☕️
           </a>
           <a
-            href="#"
+            href="/resume"
             class="btn btn-outline hover:animate-zoom-in"
           >
-            Secondary
+            Check my Resume 💼
           </a>
         </div>
       </div>
