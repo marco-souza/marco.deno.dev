30d2ced7cf49665857090bb45c2b95a917e57b19
Marco Antônio <ma.souza.junior@gmail.com>
2024-11-27 18:22:56 -0300

    fea: fetch access token

--- packages/auth/auth_test.ts
+++ packages/auth/auth_test.ts
@@ -1,5 +1,5 @@
-import { assertStringIncludes } from "@std/assert";
-import { generateRedirectUrl } from "./auth.ts";
+import { assertIsError, assertStringIncludes } from "@std/assert";
+import { fetchAuthToken, generateRedirectUrl } from "./auth.ts";
 
 Deno.test(function redirectUrlTest() {
   const origin = "http://localhost:3000";
@@ -11,4 +11,22 @@ Deno.test(function redirectUrlTest() {
   const params = new URLSearchParams();
   params.append("redirect_uri", `${origin}/api/auth/callback`);
   assertStringIncludes(redirectUrl, params.toString());
+});
+
+const code = "code";
+const state = "any";
+
+Deno.test("fetch accessToken with invalid code", async () => {
+  await fetchAuthToken(code, state).catch((error) => {
+    assertIsError(error);
+  });
+});
+
+Deno.test("throw an error if state is invalid", async () => {
+  const invalidState = "";
+
+  await fetchAuthToken(code, invalidState).catch((error) => {
+    assertIsError(error);
+    assertStringIncludes(error.message, "Invalid state");
+  });
 });
