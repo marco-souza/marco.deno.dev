e93cf47dabe4ae69d438cfd7ffe699d58127877b
Marco Antônio <ma.souza.junior@gmail.com>
2024-11-28 19:32:26 -0300

    fea: setup auth routes

--- src/server/pages.tsx
+++ src/server/pages.tsx
@@ -1,4 +1,6 @@
 import { type Context, Hono } from "hono";
+import { jsxRenderer, useRequestContext } from "hono/jsx-renderer";
+
 import { jsxMiddleware } from "~/middlewares/jsx.ts";
 import { themeMiddleware } from "~/middlewares/theme.ts";
 import ErrorPage from "~/components/ErrorPage.tsx";
@@ -7,11 +9,13 @@ import { Layout } from "~/layouts/main.tsx";
 import { github } from "~/services/github.ts";
 import { getThemeCookie } from "~/shared/theme.ts";
 
-import { jsxRenderer, useRequestContext } from "hono/jsx-renderer";
 import { ResumePage } from "~/components/ResumePage.tsx";
 import { BlogPage } from "~/components/BlogPage.tsx";
 import { BlogPostPage } from "~/components/BlogPostPage.tsx";
 import { blog } from "~/services/blog.ts";
+
+import * as auth from "@m3o/auth";
+import { raise } from "@m3o/errors";
 
 export function registerPageRoutes(app: Hono) {
   const partials = partialRouter();
@@ -20,11 +24,75 @@ export function registerPageRoutes(app: Hono) {
   const pages = pageRouter();
   app.route("/", pages);
 
+  const authRoutes = authRouter();
+  app.route("/", authRoutes);
+
   app.notFound((ctx) => {
     return ctx.render(
       <ErrorPage description="Could not find your page :(" />,
     );
   });
+}
+
+function authRouter(): Hono {
+  const routes = new Hono();
+  const { urls } = auth.configs;
+
+  routes.get("/login", (ctx) => {
+    const url = new URL(ctx.req.url);
+    const errors = url.searchParams.get("errors");
+
+    return ctx.render(
+      <div class="card shadow-md">
+        <div class="card-body">
+          <h1 class="text-2xl">Login</h1>
+
+          <form class="flex gap-4" action={urls.signIn} hx-boost="false">
+            <input
+              type="text"
+              name="username"
+              class="input input-bordered flex-1"
+              placeholder="Enter your username"
+              autofocus
+              required
+            />
+            <button class="btn btn-primary" type="submit">Login</button>
+          </form>
+
+          {errors && <p class="text-red-500">{errors}</p>}
+        </div>
+      </div>,
+    );
+  });
+
+  routes.get(urls.signIn, (ctx) => {
+    const reqUrl = new URL(ctx.req.url);
+
+    const username = reqUrl.searchParams.get("username");
+    if (username === "error") {
+      return ctx.redirect("/login?errors=Invalid username");
+    }
+
+    const authUrl = auth.generateRedirectUrl(reqUrl.origin);
+
+    console.log("User logged in", { username, authUrl });
+
+    return ctx.redirect(authUrl);
+  });
+
+  routes.get(urls.callback, async (ctx) => {
+    const reqUrl = new URL(ctx.req.url);
+    const code = reqUrl.searchParams.get("code") ?? raise("Missing code");
+    const state = reqUrl.searchParams.get("state") ?? raise("Missing state");
+
+    const token = await auth.fetchAccessToken(code, state);
+
+    console.log("User logged in", { token });
+
+    return ctx.redirect("/");
+  });
+
+  return routes;
 }
 
 function pageRouter(): Hono {
