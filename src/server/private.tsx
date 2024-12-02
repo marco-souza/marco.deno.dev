7bc870e259f7daddeef4374b58e9dff54d862713
Marco Antônio <ma.souza.junior@gmail.com>
2024-12-02 18:06:41 -0300

    fea: implement user settings page

--- src/server/private.tsx
+++ src/server/private.tsx
@@ -30,5 +30,70 @@ function privateRouter() {
     );
   });
 
+  routes.get(configs.navigation.settings, async (ctx) => {
+    const authTokenKey = ctx.get(AUTH_KEYS.authToken);
+    const profile = await github.fetchAuthenticatedProfile(authTokenKey);
+
+    return ctx.render(
+      <div class="grid grid-cols-1 gap-4 md:grid-cols-4 md:gap-8">
+        <aside class="col-span-1 flex flex-col gap-4">
+          <h1 class="text-4xl">Settings</h1>
+
+          <nav class="flex flex-col gap-4">
+            <a href="/user/settings" class="link link-primary">
+              User
+            </a>
+          </nav>
+        </aside>
+
+        <main class="col-span-3 p-4">
+          <form hx-post="/user/settings" class="grid gap-4">
+            <img
+              alt="It's Me"
+              src={profile.avatar_url}
+              class="h-40 mx-auto rounded-full"
+            />
+
+            <div class="form-group grid grid-cols-2 gap-4">
+              <div class="form-group">
+                <label for="name">Name</label>
+                <input
+                  type="text"
+                  class="input input-bordered w-full"
+                  id="name"
+                  value={profile.name}
+                />
+              </div>
+
+              <div class="form-group">
+                <label for="login">Username</label>
+                <input
+                  disabled
+                  id="login"
+                  type="text"
+                  class="input input-bordered w-full"
+                  value={profile.login}
+                />
+              </div>
+            </div>
+
+            <div class="form-group">
+              <label for="bio">Bio</label>
+
+              <textarea
+                id="bio"
+                rows={3}
+                class="textarea textarea-bordered w-full"
+                dangerouslySetInnerHTML={{ __html: profile.bio }}
+              />
+            </div>
+
+            <button type="submit" class="btn btn-primary">Save</button>
+          </form>
+        </main>
+      </div>,
+    );
+  });
+
   return routes;
 }
