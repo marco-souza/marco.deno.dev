517cc5071333c4f0dbbb90b7e49a07894494df83
Marco Antônio <ma.souza.junior@gmail.com>
2024-12-04 10:40:53 -0300

    enh: improve login page

--- src/components/LoginPage.tsx
+++ src/components/LoginPage.tsx
@@ -1,10 +1,25 @@
 import { auth } from "@m3o/auth";
+import GithubIcon from "~/components/icons/GitHubIcon.tsx";
 
 export function LoginPage({ errors = "", username = "" }) {
+  const githubButton = (
+    <div class="flex items-center gap-2">
+      <GithubIcon />
+      <span>Sign in with GitHub</span>
+    </div>
+  );
   return (
     <div class="card shadow-md max-w-96 mx-auto">
-      <div class="card-body">
-        <h1 class="text-2xl">Login</h1>
+      <div class="card-body gap-8">
+        <div className="header text-center">
+          <h1 class="text-3xl">
+            Sign in to <span class="text-primary">my lab</span>
+          </h1>
+
+          <h2 class="text-sm font-light">
+            Welcome back! Please sign in to continue
+          </h2>
+        </div>
 
         <form
           class="grid gap-4"
@@ -21,7 +36,9 @@ export function LoginPage({ errors = "", username = "" }) {
             required
           />
 
-          <button class="btn btn-primary w-full" type="submit">Login</button>
+          <button class="btn btn-outline btn-secondary w-full" type="submit">
+            {githubButton}
+          </button>
         </form>
 
         {errors && <p class="text-red-500">{errors}</p>}
