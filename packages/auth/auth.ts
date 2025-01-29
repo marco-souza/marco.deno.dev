f343f33917cc9c785be3011e3146a3b6d0087bf3
Marco Antônio <ma.souza.junior@gmail.com>
2025-01-29 12:33:45 -0300

    fea: improve gh profile types

--- packages/auth/auth.ts
+++ packages/auth/auth.ts
@@ -30,7 +30,45 @@ type RefreshAccessToken = Credentials & {
   grant_type: "refresh_token";
 };
 
-type GithubUserResponse = Record<string, unknown>;
+type GithubProfile = {
+  id: number; // GitHub IDs are typically numbers, but they might be represented as strings in some contexts
+  login: string;
+  node_id: string; // UUID (e.g., "MDQ6VXNlcjk3MzQ=")
+  avatar_url: string;
+  gravatar_id: string; // Empty string indicates no Gravatar available
+  url: string;
+  html_url: string;
+  followers_url: string;
+  following_url: string;
+  gists_url: string;
+  starred_url: string;
+  subscriptions_url: string;
+  organizations_url: string;
+  repos_url: string;
+  events_url: string; // Can have a `{privacy}` path parameter
+  received_events_url: string;
+  type: string; // In this case, it's always "User"
+  user_view_type: string; // In this case, it's always "public"
+  site_admin: boolean;
+  name: string;
+  blog: string;
+  public_repos: number; // Number of public repositories
+  public_gists: number; // Number of public gists
+  followers: number; // Number of followers
+  following: number; // Number of people being followed
+  created_at: string; // ISO 8601 date string converted to Date object
+  updated_at: string; // ISO 8601 date string converted to Date object
+  bio?: string;
+  email?: string;
+  company?: string;
+  location?: string;
+  hireable?: string; // Typically a 'yes' or 'no' string
+  twitter_username?: string;
+};
+
+interface AuthenticatedProfile extends GithubProfile {
+  email: string; // requires email access role
+}
 
 export class GitHubAuth {
   urls = {
