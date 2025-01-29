3d3954a8e8ef9e212525c51055cea1317453ff76
Marco Antônio <ma.souza.junior@gmail.com>
2025-01-29 19:00:55 -0300

    enh: add User-Agent to request headers

--- packages/auth/auth.ts
+++ packages/auth/auth.ts
@@ -1,74 +1,13 @@
 import { assert, raise } from "@m3o/errors";
-
-type Credentials = {
-  client_id: string;
-  client_secret: string;
-  scope: string;
-};
-
-export type AccessToken = {
-  access_token: string;
-  expires_in: number;
-  refresh_token: string;
-  refresh_token_expires_in: number;
-  token_type: string;
-  scope: string;
-};
-
-type AccessTokenError = {
-  error: string;
-  error_uri?: string;
-  error_description?: string;
-};
-
-type FetchAccessToken = Credentials & {
-  code: string;
-};
-
-type RefreshAccessToken = Credentials & {
-  refresh_token: string;
-  grant_type: "refresh_token";
-};
-
-type GithubProfile = {
-  id: number; // GitHub IDs are typically numbers, but they might be represented as strings in some contexts
-  login: string;
-  node_id: string; // UUID (e.g., "MDQ6VXNlcjk3MzQ=")
-  avatar_url: string;
-  gravatar_id: string; // Empty string indicates no Gravatar available
-  url: string;
-  html_url: string;
-  followers_url: string;
-  following_url: string;
-  gists_url: string;
-  starred_url: string;
-  subscriptions_url: string;
-  organizations_url: string;
-  repos_url: string;
-  events_url: string; // Can have a `{privacy}` path parameter
-  received_events_url: string;
-  type: string; // In this case, it's always "User"
-  user_view_type: string; // In this case, it's always "public"
-  site_admin: boolean;
-  name: string;
-  blog: string;
-  public_repos: number; // Number of public repositories
-  public_gists: number; // Number of public gists
-  followers: number; // Number of followers
-  following: number; // Number of people being followed
-  created_at: string; // ISO 8601 date string converted to Date object
-  updated_at: string; // ISO 8601 date string converted to Date object
-  bio?: string;
-  email?: string;
-  company?: string;
-  location?: string;
-  hireable?: string; // Typically a 'yes' or 'no' string
-  twitter_username?: string;
-};
-
-interface AuthenticatedProfile extends GithubProfile {
-  email: string; // requires email access role
-}
+import type {
+  AccessToken,
+  AccessTokenError,
+  AuthenticatedProfile,
+  Credentials,
+  FetchAccessToken,
+  GithubProfile,
+  RefreshAccessToken,
+} from "./types.ts";
 
 export class GitHubAuth {
   urls = {
@@ -84,6 +23,7 @@ export class GitHubAuth {
   private headers = {
     "Accept": "application/json",
     "Content-Type": "application/json",
+    "User-Agent": "@m3o/auth",
   };
 
   constructor(private credentials: Credentials) {}
