147b165e8c1a3c271ca6c662e722ddf1c88eb002
Marco Antônio <ma.souza.junior@gmail.com>
2025-03-24 12:26:39 -0300

    oth: add implementation todo

--- scripts/consume-download-queue.ts
+++ scripts/consume-download-queue.ts
@@ -4,6 +4,7 @@ const s = queue.strategyBuilder();
 
 s.addStrategy<{ link: string }>("download-music", ({ payload }) => {
   console.log("Download music", payload.link);
+  // TODO: download music from youtube
 });
 
 await queue.listenMessages(s);
